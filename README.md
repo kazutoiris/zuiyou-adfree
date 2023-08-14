ce69d03972106e0ec30d5a11f5f1897128fb5098
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2023-08-14 18:27:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Zuiyou ADFree
+`Issue`, `Pull Requests`, `Star`, `Fork` are all welcomed.
+
+Designed for:
+
+- ADFree (including splashAD, feedAD, commentAD)
+
+Only support `LSPosed`.
+
+Enjoy :)
+
+### Acknowledgements
+
+This module is built using [Yuki Hook API](https://github.com/fankes/YukiHookAPI).
---
+++ app/src/main/java/com/zuiyou/adfree/HookEntry.kt
@@ -0,0 +1,113 @@
+package com.zuiyou.adfree
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.log.loggerE
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+object HookEntry : IYukiHookXposedInit {
+    override fun onInit() = configs {
+        isDebug = false
+    }
+
+    override fun onHook() = encase {
+        loadApp("cn.xiaochuankeji.tieba") {
+
+//            method 1
+            "cn.xiaochuankeji.hermes.core.Hermes".hook {
+                injectMember {
+                    method {
+                        name = "getProviderList\$core_release"
+                    }
+                    afterHook {
+                        (result as MutableList<*>).clear()
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook getProviderList fail: ${it.message}")
+                }
+            }
+
+            "cn.xiaochuankeji.hermes.core.workflow.init.InitUtil".hook {
+                injectMember {
+                    method {
+                        name = "init"
+                        paramCount = 1
+                    }
+                    replaceToFalse()
+                }.onAllFailure {
+                    loggerE(msg = "Hook init fail: ${it.message}")
+                }
+            }
+
+//            method 2
+            "cn.xiaochuankeji.hermes.core.provider.ADProvider".hook {
+                injectMember {
+                    method {
+                        name = "getChannel"
+                    }
+                    beforeHook {
+                        result = 0
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook getChannel fail: ${it.message}")
+                }
+            }
+
+            "cn.xiaochuankeji.hermes.core.provider.ADProvider".hook {
+                injectMember {
+                    method {
+                        name = "getConfigKey"
+                    }
+                    beforeHook {
+                        result = ""
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook getChannel fail: ${it.message}")
+                }
+            }
+
+//            method 3
+            "cn.xiaochuankeji.hermes.core.api.entity.ADSDKConfigResponseData".hook {
+                injectMember {
+                    method {
+                        name = "getEnable"
+                    }
+                    replaceToFalse()
+                }.onAllFailure {
+                    loggerE(msg = "Hook getEnable fail: ${it.message}")
+                }
+            }
+
+//            method 4
+            "cn.xiaochuankeji.hermes.core.api.entity.ThirdSDKConfigResponse".hook {
+                injectMember {
+                    method {
+                        name = "getEnable"
+                    }
+                    replaceToFalse()
+                }.onAllFailure {
+                    loggerE(msg = "Hook getEnable fail: ${it.message}")
+                }
+            }
+
+//            method 5
+            "cn.xiaochuankeji.hermes.core.api.entity.ADConfigResponseDataKt".hook {
+                injectMember {
+                    method {
+                        name = "allRegisteredSDKConfigs"
+                        paramCount = 1
+                    }
+                    beforeHook {
+                        result = emptyMap<Any, Any>()
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook allRegisteredSDKConfigs fail: ${it.message}")
+                }
+            }
+
+        }
+    }
+
+}
