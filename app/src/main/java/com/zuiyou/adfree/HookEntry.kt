f75f9604ba1f7861a921809e7ff3b2575b2ebc03
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2024-02-21 20:46:39 +0800

    update to support the latest version

--- app/src/main/java/com/zuiyou/adfree/HookEntry.kt
+++ app/src/main/java/com/zuiyou/adfree/HookEntry.kt
@@ -6,6 +6,7 @@ import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.log.loggerE
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
+
 @InjectYukiHookWithXposed
 object HookEntry : IYukiHookXposedInit {
     override fun onInit() = configs {
@@ -15,95 +16,130 @@ object HookEntry : IYukiHookXposedInit {
     override fun onHook() = encase {
         loadApp("cn.xiaochuankeji.tieba") {
 
-//            method 1
-            "cn.xiaochuankeji.hermes.core.Hermes".hook {
+            "cn.xiaochuankeji.tieba.networking.data.VipPrivilege".hook {
                 injectMember {
                     method {
-                        name = "getProviderList\$core_release"
-                    }
-                    afterHook {
-                        (result as MutableList<*>).clear()
-                    }
-                }.onAllFailure {
-                    loggerE(msg = "Hook getProviderList fail: ${it.message}")
-                }
-            }
-
-            "cn.xiaochuankeji.hermes.core.workflow.init.InitUtil".hook {
-                injectMember {
-                    method {
-                        name = "init"
-                        paramCount = 1
-                    }
-                    replaceToFalse()
-                }.onAllFailure {
-                    loggerE(msg = "Hook init fail: ${it.message}")
-                }
-            }
-
-//            method 2
-            "cn.xiaochuankeji.hermes.core.provider.ADProvider".hook {
-                injectMember {
-                    method {
-                        name = "getChannel"
+                        name = "b"
                     }
                     beforeHook {
-                        result = 0
+                        result = true
                     }
-                }.onAllFailure {
-                    loggerE(msg = "Hook getChannel fail: ${it.message}")
                 }
                 injectMember {
                     method {
-                        name = "getConfigKey"
+                        name = "c"
                     }
                     beforeHook {
-                        result = ""
+                        result = true
                     }
                 }.onAllFailure {
-                    loggerE(msg = "Hook getConfigKey fail: ${it.message}")
+                    loggerE(msg = "Hook VipPrivilege fail: ${it.message}")
                 }
             }
 
-//            method 3
-            "cn.xiaochuankeji.hermes.core.api.entity.ADSDKConfigResponseData".hook {
+            "cn.xiaochuankeji.tieba.networking.data.MemberVipInfo".hook {
                 injectMember {
                     method {
-                        name = "getEnable"
-                    }
-                    replaceToFalse()
-                }.onAllFailure {
-                    loggerE(msg = "Hook getEnable fail: ${it.message}")
-                }
-            }
-
-//            method 4
-            "cn.xiaochuankeji.hermes.core.api.entity.ThirdSDKConfigResponse".hook {
-                injectMember {
-                    method {
-                        name = "getEnable"
-                    }
-                    replaceToFalse()
-                }.onAllFailure {
-                    loggerE(msg = "Hook getEnable fail: ${it.message}")
-                }
-            }
-
-//            method 5
-            "cn.xiaochuankeji.hermes.core.api.entity.ADConfigResponseDataKt".hook {
-                injectMember {
-                    method {
-                        name = "allRegisteredSDKConfigs"
-                        paramCount = 1
+                        name = "hasNewProfileBubble"
                     }
                     beforeHook {
-                        result = emptyMap<Any, Any>()
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasPhotoAccessory"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasProfileDialog"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasVipAvatar"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasVipBackground"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasVipDanmaku"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "hasVipPackage"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "isVip"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }
+                injectMember {
+                    method {
+                        name = "isYearVipPackage"
+                    }
+                    beforeHook {
+                        result = true
                     }
                 }.onAllFailure {
-                    loggerE(msg = "Hook allRegisteredSDKConfigs fail: ${it.message}")
+                    loggerE(msg = "Hook MemberVipInfo fail: ${it.message}")
                 }
             }
 
+            "cf".hook {
+                injectMember {
+                    method {
+                        name = "s"
+                    }
+                    beforeHook {
+                        result = true
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook cf fail: ${it.message}")
+                }
+            }
+
+            "cn.xiaochuankeji.tieba.ui.base.SplashActivity".hook {
+                injectMember {
+                    method {
+                        name = "B2"
+                    }
+                    beforeHook {
+                        loggerE(msg = args.contentToString())
+                        args[1] = android.os.Bundle()
+                    }
+                }.onAllFailure {
+                    loggerE(msg = "Hook SplashActivity fail: ${it.message}")
+                }
+            }
         }
     }
 
