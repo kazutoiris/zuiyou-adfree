792d02a66e774f85e43a95d4dd5555959c93ea86
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2024-03-01 00:11:30 +0800

    update to support the latest version

--- app/src/main/java/com/zuiyou/adfree/HookEntry.kt
+++ app/src/main/java/com/zuiyou/adfree/HookEntry.kt
@@ -16,128 +16,24 @@ object HookEntry : IYukiHookXposedInit {
     override fun onHook() = encase {
         loadApp("cn.xiaochuankeji.tieba") {
 
-            "cn.xiaochuankeji.tieba.networking.data.VipPrivilege".hook {
+            "cn.xiaochuankeji.hermes.core.provider.ADProvider".hook {
                 injectMember {
-                    method {
-                        name = "b"
+                    constructor {
+                        paramCount = 3
                     }
                     beforeHook {
-                        result = true
+                        args[1] = ""
                     }
                 }
                 injectMember {
                     method {
-                        name = "c"
+                        name = "getConfigKey"
                     }
                     beforeHook {
-                        result = true
+                        result = ""
                     }
                 }.onAllFailure {
-                    loggerE(msg = "Hook VipPrivilege fail: ${it.message}")
-                }
-            }
-
-            "cn.xiaochuankeji.tieba.networking.data.MemberVipInfo".hook {
-                injectMember {
-                    method {
-                        name = "hasNewProfileBubble"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasPhotoAccessory"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasProfileDialog"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasVipAvatar"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasVipBackground"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasVipDanmaku"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "hasVipPackage"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "isVip"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "isYearVipPackage"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }.onAllFailure {
-                    loggerE(msg = "Hook MemberVipInfo fail: ${it.message}")
-                }
-            }
-
-            "cf".hook {
-                injectMember {
-                    method {
-                        name = "s"
-                    }
-                    beforeHook {
-                        result = true
-                    }
-                }.onAllFailure {
-                    loggerE(msg = "Hook cf fail: ${it.message}")
-                }
-            }
-
-            "cn.xiaochuankeji.tieba.ui.base.SplashActivity".hook {
-                injectMember {
-                    method {
-                        name = "B2"
-                    }
-                    beforeHook {
-                        loggerE(msg = args.contentToString())
-                        args[1] = android.os.Bundle()
-                    }
-                }.onAllFailure {
-                    loggerE(msg = "Hook SplashActivity fail: ${it.message}")
+                    loggerE(it.toString())
                 }
             }
         }
